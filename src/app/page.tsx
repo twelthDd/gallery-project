74c36575d8d519030c9c14b6ddb51ebcee1694cb
TwelthDd <79723260+twelthDd@users.noreply.github.com>
2024-07-21 01:53:45 -0400

    created custo upload button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ async function Images() {
 
   return (
     <div className="flex flex-wrap justify-center gap-4 p-4">
-      {[...images, ...images, ...images].map((image) => (
+      {images.map((image) => (
         <div key={image.id} className="flex h-48 w-48 flex-col">
         <Link  key={image.id} href={`/photos/${image.id}`} passHref legacyBehavior={false}>
             <Image
